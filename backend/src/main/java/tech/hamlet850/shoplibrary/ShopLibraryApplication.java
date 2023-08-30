9cf930409115afa0e6e83d514f69710077f83f26
NazarovIlya <eliakohai@gmail.com>
2023-08-30 17:17:37 +0300

    project init

---
+++ backend/src/main/java/tech/hamlet850/shoplibrary/ShopLibraryApplication.java
@@ -0,0 +1,13 @@
+package tech.hamlet850.shoplibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopLibraryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tech/hamlet850/shoplibrary/ShopLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.hamlet850.shoplibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
