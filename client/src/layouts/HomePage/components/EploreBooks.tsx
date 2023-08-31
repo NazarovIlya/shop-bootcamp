a494ba35e182d1e717f5cfc3d1bcf52a38ac7f7f
NazarovIlya <eliakohai@gmail.com>
2023-09-01 00:17:48 +0300

    ExploreBooks component complited

--- client/src/layouts/HomePage/components/EploreBooks.tsx
+++ client/src/layouts/HomePage/components/EploreBooks.tsx
@@ -1,3 +1,20 @@
-function ExloreBooks() {}
+import Carousel from "./Carousel";
+
+function ExloreBooks() {
+  return (
+    <div className="p-2 mb-4 bg-dark header">
+      <div className="container-fluid py-3 text-white d-flex justify-content-center aling-item-center">
+        <div>
+          <h1 className="display-5">Источник</h1>
+          <p className="display-10">знаний</p>
+          <a href="/" type="button" className="btn btn-primary main-color">
+            Избранные книги
+          </a>
+        </div>
+      </div>
+      <Carousel />
+    </div>
+  );
+}
 
 export default ExloreBooks;
