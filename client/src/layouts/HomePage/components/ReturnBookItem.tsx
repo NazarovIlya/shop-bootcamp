da738a0858a97d8391abf1e8cf6d4ac9f25f87e7
NazarovIlya <eliakohai@gmail.com>
2023-08-31 23:37:46 +0300

    ReturnBookItem componrnt complited

---
+++ client/src/layouts/HomePage/components/ReturnBookItem.tsx
@@ -0,0 +1,20 @@
+function ReturnBookItem() {
+  return (
+    <div className="col-xs-6 col-sm-6 colmd-4 col-lg-3 mb-3">
+      <div className="text-center">
+        <img
+          className="image-carousel"
+          src={require("./../../../Images/ImageDecor/image-01.png")}
+          alt="book_photo"
+        />
+        <h6>Книга</h6>
+        <p>Наименование</p>
+        <a className="btn main-color text-white" href="/">
+          Текст 1
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default ReturnBookItem;
