975c39239ae221655a945c8c01de730c2700fd88
NazarovIlya <eliakohai@gmail.com>
2023-09-04 01:18:37 +0300

    host settings added

---
+++ backend/src/main/java/tech/hamlet850/shoplibrary/env/Settings.java
@@ -0,0 +1,11 @@
+package tech.hamlet850.shoplibrary.env;
+
+final public class Settings {
+
+  private static String HOST = "http://localhost:3000";
+
+  public static String getHOST() {
+    return HOST;
+  }
+
+}
