58af850665dfb8f75cf7e5f8b9cf0004d28c4e9e
NazarovIlya <eliakohai@gmail.com>
2023-09-09 23:26:59 +0300

    router settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import "./App.css";
 import NavigationBar from "./layouts/NavigationAndFooter/NavigationBar";
 import HomePage from "./layouts/HomePage/HopePage";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Footer from "./layouts/HomePage/components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="d-flex flex-column min-vh-100">
       <NavigationBar />
-      <HomePage />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home">
+          <HomePage />
+        </Route>
+        <Route path="/search"></Route>
+      </Switch>
+      <Footer />
     </div>
   );
 }
